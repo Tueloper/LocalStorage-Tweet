cd446b923b3849d78adb65eefcdd667eba1af442
Tueloper <ozurumbatochuku@yahoo.com>
2019-07-30 05:43:40 +0100

    Project Completed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
  // Variables
 const show = document.querySelector('#tweet-list');
 const storage = [];
-console.log(show.classList)
+// console.log(show.classList)
 
  // Event Listeners
  eventListeners();
@@ -12,6 +12,9 @@ console.log(show.classList)
 
    //remove the element
    show.addEventListener('click', deleteLi)
+
+   //autommatic loading of content from Dom
+   document.addEventListener('DOMContentLoaded', loadedStorage)
  }
   
  // Functions
@@ -31,21 +34,29 @@ console.log(show.classList)
    removeBtn.classList = 'fe fe-close';
    li.appendChild(removeBtn)
 
-//    //storing the values
-//    storage.push(li);
-//    localStorage.setItem(storage)
-
     //displaying on page
     show.appendChild(li)
     
     addTweetStorage(tweet);
+
+    //notification
+    alert('Tweet Added')
+
+    return this.reset();
  }
 
  //deleteLi Functioln
 function deleteLi(e) {
     if (e.target.classList.contains('fe-close')) {
+        let tweets = getTweetFromStorage()
+        
+        //remove tweet from the display
         e.target.parentElement.remove();
     }
+    
+        
+    //seleting the element to be deleted
+    removeTweetStorage(e.target.parentElement.textContent);
 }
  
 //adding tweet
@@ -70,4 +81,39 @@ function getTweetFromStorage() {
         tweets = JSON.parse(stateLs)
     }
     return tweets;
+}
+
+function loadedStorage() {
+    let tweets = getTweetFromStorage()
+
+    tweets.forEach(tweet => {
+        //creating an li
+        const li = document.createElement('li');
+        li.classList = 'flex';
+        li.textContent = tweet;
+
+        //how to remove a btn
+        const removeBtn = document.createElement('i');
+        removeBtn.classList = 'fe fe-close';
+        li.appendChild(removeBtn)
+
+        //displaying on page
+        show.appendChild(li)
+    });
+}
+
+function removeTweetStorage(tweet) {
+    
+    let tweets = getTweetFromStorage()
+
+    let tweetDel = tweet
+
+    tweets.forEach((tweetLs, index) => {
+        if (tweetDel === tweetLs ) {
+            // console.log('yes')
+            tweets.splice(index, 1)
+        }
+    });
+
+    localStorage.setItem('tweets', JSON.stringify(tweets))
 }
